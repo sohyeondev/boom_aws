4ad3183ae5eb032534ee44a644928db630875018
sohyeondev <sohyeondev@gmail.com>
2021-09-03 16:30:40 +0900

    hash

--- server/routes/home.js
+++ server/routes/home.js
@@ -2,18 +2,21 @@
 var express = require("express");
 var router = express.Router(); 
 var mysql= require("./mysql");
+const crypto = require('crypto');
 
 router.post('/', (req, res) =>{
 
     var loginEmail = req.body.loginEmail  //클라이언트에서 받아오는 값
     var loginPW = req.body.loginPW
 
-    mysql.query('SELECT email, pw FROM users', 
+    mysql.query('SELECT email, pw, salt FROM users', 
         function(error, result){
             if(!error){
                 for (var i=0; i<result.length; i++) {
                     if(loginEmail === result[i].email) {
-                        if(loginPW === result[i].pw) {
+                        var userSalt = result[i].salt
+                        const hashLoginPW = crypto.createHash('sha512').update(loginPW + userSalt).digest('hex');
+                        if(hashLoginPW === result[i].pw) {
                             return res.json({ message: true }) //클라이언트에 전달
                         }
                         else {
--- server/routes/signup.js
+++ server/routes/signup.js
@@ -2,20 +2,25 @@
 var express = require("express");
 var router = express.Router(); 
 var mysql= require("./mysql");
+const crypto = require('crypto');
+const salt = crypto.randomBytes(128).toString('base64');
 
 router.post('/', (req, res) =>{
 
     var name = req.body.name //클라이언트에서 받아오는 값
     var email = req.body.email
     var pw = req.body.pw
+    var userSalt = salt
+
+    const hashPW = crypto.createHash('sha512').update(pw + salt).digest('hex');
 
     mysql.query("SELECT * FROM users WHERE email = ?",
     [email],
     function(error, result){
         if(!error) {
             if(result[0]==undefined){
-                mysql.query('INSERT INTO users(name, email, pw) VALUES(?, ?, ?);',
-                [name, email, pw],
+                mysql.query('INSERT INTO users(name, email, pw, salt) VALUES(?, ?, ?, ?);',
+                [name, email, hashPW, userSalt],
                 function(error, result){
                     if(!error){
                         res.json({ message: true }) //클라이언트에 전달
