e988bfd560d6cfb8477de06075353086e56a4024
jiyeon <85976276+luvoul@users.noreply.github.com>
2021-09-04 11:20:42 +0900

    new

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import laco from "../../assets/img/lacotaco.png";
-import axios from 'axios';
+import logo from "../../assets/img/boom.png";
+import axios from "axios";
 
 function Home({ history }) {
   const [loginEmail, setLoginEmail] = useState("");
@@ -9,10 +9,10 @@ function Home({ history }) {
 
   const onLoginEmailHandler = (event) => {
     setLoginEmail(event.currentTarget.value);
-  }
+  };
   const onLoginPasswordHandler = (event) => {
     setLoginPassword(event.currentTarget.value);
-  }
+  };
 
   useEffect(() => {
     const p = document.getElementById("notEmail");
@@ -32,60 +32,69 @@ function Home({ history }) {
 
   const onLoginSubmitHandler = (event) => {
     event.preventDefault();
-    Post(loginEmail, loginPassword)
-  }
+    Post(loginEmail, loginPassword);
+  };
 
-  const Post = async (email, pw)=> {
-    if ( email === "" || pw === "" ) {
-      alert("모든 정보를 기입해주세요.")
+  const Post = async (email, pw) => {
+    if (email === "" || pw === "") {
+      alert("모든 정보를 기입해주세요.");
+    } else if (email.indexOf("@") === -1) {
+      alert("이메일 형식을 확인해주세요.");
+    } else {
+      console.log("aaa");
+      await axios
+        .post(`http://127.0.0.1:3001/`, {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          loginEmail: email,
+          loginPW: pw,
+        })
+        .then((res) => {
+          //로그인 성공
+          if (res.data.message === true) {
+            alert("로그인 성공! 인증페이지로 이동합니다.");
+            history.push("/auth");
+          }
+          //로그인 실패
+          else if (res.data.message === false) {
+            alert("이메일과 비밀번호를 확인해주세요.");
+          } else if (res.data.message === "notUser") {
+            alert("가입된 정보가 없습니다.");
+          }
+        })
+        .catch((error) => {
+          console.log("클라이언트 오류 : " + error);
+        });
     }
-    else if (email.indexOf("@") === -1) {
-      alert("이메일 형식을 확인해주세요.")
-    } 
-    else {
-      console.log("aaa")
-      await axios.post(`http://127.0.0.1:3001/`, {
-        headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json',
-        },
-        loginEmail: email,
-        loginPW: pw,
-    }).then((res) => {
-        //로그인 성공
-        if(res.data.message === true){
-          alert('로그인 성공! 인증페이지로 이동합니다.')
-          history.push('/auth');
-        }
-        //로그인 실패
-        else if(res.data.message === false){
-          alert('이메일과 비밀번호를 확인해주세요.')
-        }
-        else if(res.data.message === "notUser") {
-          alert('가입된 정보가 없습니다.')
-        }
-    })
-    .catch((error)=> {
-        console.log("클라이언트 오류 : " + error)
-    })
-    }
-  }
-  
+  };
+
   return (
     <>
       <div className="all">
         <div className="whtthe">
-          <img src={laco} alt="라코타코" width="70px" height="65px" />
-          <div>
-            화상회의, 라코타코와 함께하시면 <br />더 쉽게 할 수 있습니다.
-          </div>
+          <img src={logo} alt="로고" width="160px" height="85px" />
         </div>
-
+        <br />
         <div className="login">
-          <input type="text" id="id" size="15" onChange={onLoginEmailHandler} placeholder="이메일" />
+          <br />
+          <input
+            type="text"
+            id="id"
+            size="15"
+            onChange={onLoginEmailHandler}
+            placeholder="이메일"
+          />
           <br />
           <p id="notEmail">이메일 형식으로 입력하세요!</p>
-          <input type="password" id="password" size="15" onChange={onLoginPasswordHandler} placeholder="비밀번호" />
+          <input
+            type="password"
+            id="password"
+            size="15"
+            onChange={onLoginPasswordHandler}
+            placeholder="비밀번호"
+          />
           <br />
           <br />
           <Link to="/auth">
@@ -93,21 +102,19 @@ function Home({ history }) {
               type="submit"
               className="button"
               onClick={onLoginSubmitHandler}
-            >로그인</button>
+            >
+              로그인
+            </button>
           </Link>
           <div>
             <br />
-            <Link to="/sigunp">
-              비밀번호를 잊으셨나요?
-            </Link>
+            <Link to="/sigunp">비밀번호를 잊으셨나요?</Link>
           </div>
+          <br />
           <hr />
+          <br />
           <Link to="/signup">
-            <input
-              type="button"
-              className="button2"
-              value="새 계정 만들기"
-            />
+            <input type="button" className="button2" value="새 계정 만들기" />
           </Link>
         </div>
       </div>
